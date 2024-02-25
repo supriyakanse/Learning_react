57a4d863d3b924d1e1bec66f37733bc18085a366
supriya kanse <supriyak8403@gmail.com>
2024-02-25 14:29:08 +0530

    components of react

---
+++ linkcode-react/src/Footer.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+let dayCount = 1;
+function Footer(){
+    return <p>{`we are into day ${dayCount} of react learnings !`}</p>;
+}
+export default Footer;
+
---
+++ linkcode-react/src/Header.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+
+function Header(){
+    return <h1>Welcome to Linkcode</h1>;
+}
+export default Header;
